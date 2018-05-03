6b71fbd87bfecc829b46ab2371bc401842cb3deb
Juan Pablo Djeredjian <jpdjeredjian@gmail.com>
2018-05-03 01:07:13 -0300

    - Removed AT

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,6 @@ const express = require('express');
 const router = express.Router();
 const octokit = require('@octokit/rest')()
 
-octokit.authenticate({
-  type: 'token',
-  token: 'fecc3c2ecea274de69d048b884f53a5c38dd3c16'
-})
-
 router.get('/searchRepos', async (req,res,next) => {
   let {search} = req.query;
   const result = await octokit.search.repos({q:search,sorts:"stars",order:"desc"})
